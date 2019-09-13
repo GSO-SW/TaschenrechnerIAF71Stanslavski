3fff6ab5dd940784800cfe002c27979145361711
Chris-Beeb <54985132+Chris-Beeb@users.noreply.github.com>
2019-09-13 13:05:18 +0200

    + rechnen programmiert

--- Taschenrechner/Taschenrechner/Program.cs
+++ Taschenrechner/Taschenrechner/Program.cs
@@ -10,6 +10,31 @@ namespace Taschenrechner
     {
         static void Main(string[] args)
         {
+            string antwort;
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Wilkommen zu dem besten Taschenrechner!");
+                Console.WriteLine("Geben sie ihre Zahlen jetzt ein :");
+
+
+                int zahl1;
+                int zahl2;
+
+                Console.WriteLine("Erste Zahl: ");
+                zahl1 = Convert.ToInt32(Console.ReadLine());
+                zahl2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Das Ergebnis ist : {0}" , zahl1 + zahl2);
+
+                Console.WriteLine("Nochmal rechnen? Ja/Nein");
+
+                antwort = Console.ReadLine();
+
+            } while (antwort == "Ja" || antwort == "J" || antwort == "j" || antwort == "ja");
+
 
 
 
